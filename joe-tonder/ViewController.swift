ec56da20089c3bce5befc2b416ac49ab80f81e39
Joe Sloan <joe@joesloandesign.com>
2017-12-11 11:32:12 -0700

    Adds swipe animations and a basic login screen layout

--- joe-tonder/ViewController.swift
+++ joe-tonder/ViewController.swift
@@ -12,21 +12,56 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swipeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let object = PFObject(className: "Testing")
-        object["Foo"] = "Bar"
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(wasDragged(gestureRecognizer: )))
+        swipeLabel.addGestureRecognizer(gesture)
         
-        let terminalCommand = PFObject(className: "terminalCommand")
-        terminalCommand["command"] = "parse-dashboard --appId joeTonder --masterKey joeTonder --serverURL \"http://joe-tonder.herokuapp.com/parse\""
-        
-        terminalCommand.saveInBackground { (success, error) in
-            print("Object has been saved")
-        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer){
+        let labelPoint = gestureRecognizer.translation(in: view)
+        swipeLabel.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
+        
+        let xFromCenter = view.bounds.width/2 - swipeLabel.center.x
+        
+        let rotation = CGAffineTransform(rotationAngle: -(xFromCenter/400))
+        var scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/700, y: 1 + xFromCenter/700)
+
+        
+        if xFromCenter > 0 {
+            scaledAndRotated = rotation.scaledBy(x: 1 - xFromCenter/900, y: 1 - xFromCenter/900)
+        }else{
+            scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/900, y: 1 + xFromCenter/900)
+        }
+        
+        
+        let resetRotation = CGAffineTransform(rotationAngle: 0)
+        
+        let resetAll = resetRotation.scaledBy(x: 1, y: 1)
+        
+        swipeLabel.transform = scaledAndRotated
+        
+        if gestureRecognizer.state == .ended{
+            if swipeLabel.center.x < view.bounds.width/2 - 100 {
+                print("Not Interested")
+            }
+            
+            if swipeLabel.center.x > view.bounds.width/2 + 100 {
+                print("Interested")
+            }
+            
+            swipeLabel.center = self.view.center
+            swipeLabel.transform = resetAll
+            
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
