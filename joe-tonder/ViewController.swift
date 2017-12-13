f2bc8912c1dba725d0039b6fde44ced14ec005f5
Joe Sloan <joe@joesloandesign.com>
2017-12-13 13:44:44 -0700

    Adds a working sort function to Swipe that logs a users decision

--- joe-tonder/ViewController.swift
+++ joe-tonder/ViewController.swift
@@ -11,6 +11,8 @@ import Parse
 
 
 class ViewController: UIViewController {
+    
+    var displayUserID = ""
 
     
     @IBOutlet var matchImageView: UIImageView!
@@ -24,6 +26,67 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "logOutFromCards", sender: nil)
     }
     
+    
+    @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer){
+        
+        let labelPoint = gestureRecognizer.translation(in: view)
+        matchImageView.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
+        
+        let xFromCenter = view.bounds.width/2 - matchImageView.center.x
+        
+        let rotation = CGAffineTransform(rotationAngle: -(xFromCenter/400))
+        var scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/700, y: 1 + xFromCenter/700)
+        
+        
+        if xFromCenter > 0 {
+            scaledAndRotated = rotation.scaledBy(x: 1 - xFromCenter/900, y: 1 - xFromCenter/900)
+        }else{
+            scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/900, y: 1 + xFromCenter/900)
+        }
+        
+        
+        let resetRotation = CGAffineTransform(rotationAngle: 0)
+        
+        let resetAll = resetRotation.scaledBy(x: 1, y: 1)
+        
+        matchImageView.transform = scaledAndRotated
+        
+        if gestureRecognizer.state == .ended{
+            
+            var acceptedOrRejected = ""
+            
+            if matchImageView.center.x < view.bounds.width/2 - 100 {
+                print("Not Interested")
+                acceptedOrRejected = "rejected"
+            }
+            
+            if matchImageView.center.x > view.bounds.width/2 + 100 {
+                print("Interested")
+                acceptedOrRejected = "accepted"
+            }
+            
+            if acceptedOrRejected != "" && displayUserID != ""{
+                PFUser.current()?.addUniqueObject(displayUserID, forKey: acceptedOrRejected)
+                PFUser.current()?.saveInBackground(block: { (success, error) in
+                    if success{
+                        print("imageUpdated")
+                        self.updateImage()
+                    }
+                })
+                
+                
+            }
+            
+            
+            
+            
+            matchImageView.center = self.view.center
+            matchImageView.transform = resetAll
+            
+        }
+        
+    }
+    
     func updateImage() {
         
         if let query = PFUser.query(){
@@ -35,16 +98,33 @@ class ViewController: UIViewController {
                 query.whereKey("isInterestedInWomen", equalTo: isFemale)
             }
             
+            var ignoredUsers: [String] = []
+            
+            if let acceptedUsers = PFUser.current()?["accepted"] as? [String]{
+                ignoredUsers += acceptedUsers
+            }
+            
+            if let rejectedUsers = PFUser.current()?["rejected"] as? [String]{
+                ignoredUsers += rejectedUsers
+            }
+            
+            query.whereKey("objectId", notContainedIn: ignoredUsers)
+            
             query.limit = 1
             
             query.findObjectsInBackground { (objects, error) in
                 if let users = objects {
+                    print("I found some objects")
                     for object in users{
                         if let user = object as? PFUser{
                             if let imageFile = user["Photo"] as? PFFile{
                                 imageFile.getDataInBackground(block: { (data, error) in
                                     if let image = data {
+                                        print("There was image data for \(PFUser.current()?.objectId)")
                                         self.matchImageView.image = UIImage(data: image)
+                                        if let objectID = object.objectId {
+                                            self.displayUserID = objectID
+                                        }
                                     }
                                 })
                             }
@@ -69,44 +149,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer){
-        let labelPoint = gestureRecognizer.translation(in: view)
-        matchImageView.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
-        
-        let xFromCenter = view.bounds.width/2 - matchImageView.center.x
-        
-        let rotation = CGAffineTransform(rotationAngle: -(xFromCenter/400))
-        var scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/700, y: 1 + xFromCenter/700)
-
-        
-        if xFromCenter > 0 {
-            scaledAndRotated = rotation.scaledBy(x: 1 - xFromCenter/900, y: 1 - xFromCenter/900)
-        }else{
-            scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/900, y: 1 + xFromCenter/900)
-        }
-        
-        
-        let resetRotation = CGAffineTransform(rotationAngle: 0)
-        
-        let resetAll = resetRotation.scaledBy(x: 1, y: 1)
-        
-        matchImageView.transform = scaledAndRotated
-        
-        if gestureRecognizer.state == .ended{
-            if matchImageView.center.x < view.bounds.width/2 - 100 {
-                print("Not Interested")
-            }
-            
-            if matchImageView.center.x > view.bounds.width/2 + 100 {
-                print("Interested")
-            }
-            
-            matchImageView.center = self.view.center
-            matchImageView.transform = resetAll
-            
-        }
-        
-    }
+    
 
 
 
