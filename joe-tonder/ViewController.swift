5e27811e716873b56c5991bf3338bbf9951b3bf7
Joe Sloan <joe@joesloandesign.com>
2017-12-12 18:15:36 -0700

    Adds an imageView instead of a label to the swipng screen.

--- joe-tonder/ViewController.swift
+++ joe-tonder/ViewController.swift
@@ -12,7 +12,9 @@ import Parse
 
 class ViewController: UIViewController {
 
-    @IBOutlet var swipeLabel: UILabel!
+    
+    @IBOutlet var matchImageView: UIImageView!
+    
     
     @IBAction func updateProfilePressed(_ sender: Any) {
         performSegue(withIdentifier: "goToUpdateView", sender: nil)
@@ -26,16 +28,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let gesture = UIPanGestureRecognizer(target: self, action: #selector(wasDragged(gestureRecognizer: )))
-        swipeLabel.addGestureRecognizer(gesture)
+        matchImageView.addGestureRecognizer(gesture)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer){
         let labelPoint = gestureRecognizer.translation(in: view)
-        swipeLabel.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
+        matchImageView.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
         
-        let xFromCenter = view.bounds.width/2 - swipeLabel.center.x
+        let xFromCenter = view.bounds.width/2 - matchImageView.center.x
         
         let rotation = CGAffineTransform(rotationAngle: -(xFromCenter/400))
         var scaledAndRotated = rotation.scaledBy(x: 1 + xFromCenter/700, y: 1 + xFromCenter/700)
@@ -52,28 +54,24 @@ class ViewController: UIViewController {
         
         let resetAll = resetRotation.scaledBy(x: 1, y: 1)
         
-        swipeLabel.transform = scaledAndRotated
+        matchImageView.transform = scaledAndRotated
         
         if gestureRecognizer.state == .ended{
-            if swipeLabel.center.x < view.bounds.width/2 - 100 {
+            if matchImageView.center.x < view.bounds.width/2 - 100 {
                 print("Not Interested")
             }
             
-            if swipeLabel.center.x > view.bounds.width/2 + 100 {
+            if matchImageView.center.x > view.bounds.width/2 + 100 {
                 print("Interested")
             }
             
-            swipeLabel.center = self.view.center
-            swipeLabel.transform = resetAll
+            matchImageView.center = self.view.center
+            matchImageView.transform = resetAll
             
         }
         
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
