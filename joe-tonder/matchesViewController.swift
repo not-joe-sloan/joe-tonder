f6a6cfb9f4c4a01cdd7c29478817e7bfa83ff3fd
Joe Sloan <joe@joesloandesign.com>
2017-12-14 18:44:26 -0700

    adds relevant match photo to tableViewCell using a PFQuery

--- joe-tonder/matchesViewController.swift
+++ joe-tonder/matchesViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class matchesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    var matchImages: [UIImage] = []
+    var userIds: [String] = []
+    
     @IBOutlet var tableView: UITableView!
     @IBAction func backTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -20,19 +24,49 @@ class matchesViewController: UIViewController, UITableViewDataSource, UITableVie
         tableView.dataSource = self
         tableView.delegate = self
         
-
+        if let query = PFUser.query() {
+            query.whereKey("accepted", contains: PFUser.current()?.objectId)
+            
+            if let acceptedPeeps = PFUser.current()?["accepted"] as? [String]{
+                query.whereKey("objectId", containedIn: acceptedPeeps)
+                query.findObjectsInBackground(block: { (objects, error) in
+                    if let users = objects {
+                        for user in users {
+                            if let theUser = user as? PFUser {
+                                if let imageFile = theUser["Photo"] as? PFFile {
+                                    imageFile.getDataInBackground(block: { (data, error) in
+                                        if let imageData = data {
+                                            print("There's data")
+                                            self.matchImages.append(UIImage(data: imageData)!)
+                                            if let objectId = theUser.objectId{
+                                                self.userIds.append(objectId)
+                                                self.tableView.reloadData()
+                                            }
+                                            
+                                        }
+                                    })
+                                }
+                            }
+                        }
+                    }
+                })
+            }
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return matchImages.count
         
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "matchCell", for: indexPath) as? matchTableViewCell{
             
-            cell.messageLabel.text = "Wazzup"
+            cell.messageLabel.text = "No sexy messages for U, loner.  You'll never get laid."
+            cell.profileImageView.image = matchImages[indexPath.row]
             return cell
         }
         return UITableViewCell()
@@ -43,5 +77,5 @@ class matchesViewController: UIViewController, UITableViewDataSource, UITableVie
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
